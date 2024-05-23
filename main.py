9dfab044883b1a991a24606d0ead58471f01db00
Eyas Ziedan <66919391+EyasZ@users.noreply.github.com>
2024-05-23 22:32:59 +0300

    working version

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from one_inch import OneInchAPI  # Ensure this is correctly implemented
 from objects import Chain, Token, TokenBinaryTree
 import sys
 
+
 class TradingBot:
-    def __init__(self, secrets ,budget=10000, chain_ids=None, interval=30, api_key="YOUR_API_KEY"):
+    def __init__(self, secrets_json, budget=0, chain_ids=None, interval=30, api_key="YOUR_API_KEY"):
         self.budget = budget
         self.chain_ids = chain_ids if chain_ids is not None else {}
         self.interval = interval
         self.logging = None
-        self.one_inch_api = OneInchAPI(api_key)
+        self.one_inch_api = OneInchAPI(secrets_json)
         self.configure_logging()
         self.chains = {name: Chain((chain_id, '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 'ORACLE_ADDRESS', name, end_point))
                        for name, (chain_id, end_point) in self.chain_ids.items()}
@@ -22,9 +23,9 @@ class TradingBot:
         self.last_pulse = {}
         self.counter = 1
         self.native_token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-        self.secrets = secrets
-        self.wallet_address = secrets['one_inch']['wallet_address']
-        self.private_key = secrets['one_inch']['wallet_pk']
+        self.secrets = secrets_json
+        self.wallet_address = secrets_json['one_inch']['wallet_address']
+        self.private_key = secrets_json['one_inch']['wallet_pk']
 
     def configure_logging(self):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='trading_bot.log', filemode='w')
@@ -48,14 +49,19 @@ class TradingBot:
             time.sleep(self.interval)
 
     def initialize_tokens(self, chain_name, chain_id):
-        raw_tokens = json.loads(self.one_inch_api.get_chain_pairs(chain_id))
+        if self.one_inch_api.end_point is None:
+            self.one_inch_api.end_point = self.secrets['quick_node'][chain_name]
+            self.one_inch_api.chain_id = chain_id
+            self.one_inch_api.logging = self.logging
+        raw_tokens = json.loads(self.one_inch_api.get_chain_pairs())
+        time.sleep(1)
         for token_id, token_info in raw_tokens.items():
             if token_id == '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee':  # Skip native currency
                 continue
 
             token = self.tokens_per_chain[chain_name].find_token(token_id)
             if token is None:
-                current_price_info = self.one_inch_api.get_swap_rate(token_id, network=chain_id)
+                current_price_info = self.one_inch_api.get_swap_rate(token_id)
                 time.sleep(1)
                 if current_price_info and 'price' in current_price_info:
                     current_price = int(current_price_info['price'])  # Ensure float for accurate calculations
@@ -86,7 +92,7 @@ class TradingBot:
 
     def update_token_scores(self, chain_name, chain_id):
         for token_id, token in self.tokens_per_chain[chain_name].tokens_map.items():
-            current_price_info = self.one_inch_api.get_swap_rate(token_id, network=chain_id)
+            current_price_info = self.one_inch_api.get_swap_rate(token_id)
             time.sleep(1)
             if current_price_info and 'price' in current_price_info:
                 current_price = int(current_price_info['price'])
@@ -103,21 +109,21 @@ class TradingBot:
                 elif price_difference < 0 < strikes:
                     strikes = 0
                 if token.id in self.trading_dict and strikes > 2 or token.id in self.trading_dict and token.score < 0:
-                    self.trading_dict.pop(token.id)
+                    if token.id != self.native_token:
+                        self.trading_dict.pop(token.id)
+                    '''elif token.id == self.native_token and strikes > 7:
+                        self.sell_all'''
                 elif token.score < 3 and token.id not in self.trading_dict:
                     self.trading_dict[token.id] = token
-                self.manage_trading_dict(chain_name, chain_id)
                 self.logging.info(f"Updated token: {token}\n ROI: {-1 * price_difference / token.initial_price}.")
 
     def check_last_pulse(self, chain_id):
         try:
-            last_pulse = self.one_inch_api.check_wallet_assets(self.wallet_address, chain_id)
-            return last_pulse
+            last_pulse = self.one_inch_api.check_wallet_assets()
+            return {address: balance for address, balance in last_pulse.items() if int(balance) != 0}
         except Exception as e:
             print(e)
         return None
-
-
 
     def manage_trading_dict(self, chain_name, chain_id):
         last_pulse = self.check_last_pulse(chain_id)
@@ -129,46 +135,23 @@ class TradingBot:
 
         # If the wallet contains assets, and there are assets in trading_dict
         if last_pulse and self.trading_dict:
+            # Check for tokens in trading_dict that are not in last_pulse (new tokens to buy)
+            for address, token in self.trading_dict.items():
+                if address not in pulse_addresses:
+                    if not token.tested:
+                        token.tested = True
+                        token.white_listed = self.one_inch_api.whitelist_token(address)
+                    if token.white_listed:
+                        to_buy.append(address)
+
             # Iterate over each asset in the last_pulse
             for address, balance in last_pulse.items():
                 token = self.trading_dict.get(address)
-
-                if not token:
-                    current_price_info = self.one_inch_api.get_swap_rate(address, network=chain_id)
-                    time.sleep(1)
-                    token_info = self.one_inch_api.get_token_info(address, chain_id)
-                    time.sleep(1)
-                    if current_price_info and 'price' in current_price_info:
-                        current_price = int(current_price_info['price'])  # Ensure float for accurate calculations
-                        market_cap = self.get_market_cap(address, chain_id)
-                    token_name = token_info['name']
-                    token_symbol = token_info['symbol']
-                    decimals = token_info['decimals']
-                    initial_score = self.calculate_initial_score(market_cap)
-                    token = Token((address, chain_id, token_name, token_symbol, decimals), initial_score,
-                                  current_price)
-
-                if not token.tested:
-                    token.tested = True
-                    token.white_listed = self.one_inch_api.whitelist_token(address)
-
-                if token.white_listed:
-                    # Check if the token is in the trading_dict or is the native token
-                    if address not in self.trading_dict and address != self.native_token:
-                        # If not, mark this token for selling
-                        to_sell.append((address, balance))
-                    elif address in self.trading_dict:
-                        # If it is in trading_dict, check if balance needs adjustment
-                        desired_balance = self.trading_dict[address].desired_balance
-                        if balance > desired_balance:
-                            to_sell.append((address, balance - desired_balance))
-                        elif balance < desired_balance:
-                            to_buy.append((address, desired_balance - balance))
-
-            # Check for tokens in trading_dict that are not in last_pulse (new tokens to buy)
-            for address, token in self.trading_dict.items():
-                if address not in pulse_addresses and token.white_listed:
-                    to_buy.append((address, token.desired_balance))
+                if token:
+                    continue
+                else:
+                    # If the address is in last_pulse but not in trading_dict, mark for selling
+                    to_sell.append((address, balance))
 
         self.logging.info(f"to sell: {to_sell}\n to buy: {to_buy}")
 
@@ -177,15 +160,14 @@ class TradingBot:
     def bridge(self, token_id, amount):
         self.logging.info(f"Dummy swap {token_id} for native currency with amount {amount}")
 
-
-    def swap_token_for_native(self, token, chain, amount):
-        tx_hash = self.one_inch_api.swap_tokens(self.wallet_address, self.private_key, token.id,
-                                                self.native_token, amount, chain.endpoint)
+    def swap_token_for_native(self, token, amount):
+        tx_hash = self.one_inch_api.swap_tokens(self.wallet_address, token.id,
+                                                self.native_token, amount)
         self.logging.info(f"swapped {token.id} for native currency with amount {amount}\n transaction hash = {tx_hash}")
 
-    def swap_native_for_token(self, token, chain, amount):
-        tx_hash = self.one_inch_api.swap_tokens(self.wallet_address, self.private_key,
-                                                self.native_token, token.id, amount, chain.endpoint)
+    def swap_native_for_token(self, token, amount):
+        tx_hash = self.one_inch_api.swap_tokens(self.wallet_address,
+                                                self.native_token, token.id, amount)
         self.logging.info(f"Dummy swap native currency for {token.id} with amount {amount}\n"
                           f"transaction hash = {tx_hash}")
 
--- one_inch.py
+++ one_inch.py
@@ -1,3 +1,4 @@
+import time
 from decimal import Decimal
 import requests
 from quick_node import Web3Instance
@@ -5,61 +6,39 @@ from quick_node import Web3Instance
 
 class OneInchAPI:
 
-    def __init__(self, api_key: str):
+    def __init__(self, secrets):
         """
         Initializes the OneInchAPI with a given API key.
 
         :param api_key: Your 1inch API key.
         """
-        self.wallet_address = "0x9055192d0673CE6034b302a9921A3E071A220553"
-        self.api_key = api_key
+        self.wallet_address = secrets['one_inch']['wallet_address']
+        self.api_key = secrets['one_inch']['api_key']
+        self.end_point = None
         self.logging = None
+        self.chain_id = None
         self.native_token = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+        self.private_key = secrets['one_inch']['wallet_pk']
 
-    def whitelist_token(self, token_address):
-        small_amount = 0.01  # Adjust this value as necessary
+    def whitelist_token(self, address):
+        web3_instance = Web3Instance.get_instance(self.end_point).web3
+        amount = web3_instance.toWei(1, 'ether')
         try:
-            # Example 1inch API endpoints, adjust as necessary
-            sell_url = f"https://api.1inch.exchange/v3.0/1/swap?fromTokenAddress={token_address}&toTokenAddress={self.native_token}&amount={small_amount}&fromAddress=YOUR_WALLET_ADDRESS&slippage=1"
-            buy_url = f"https://api.1inch.exchange/v3.0/1/swap?fromTokenAddress={self.native_token}&toTokenAddress={token_address}&amount={small_amount}&fromAddress=YOUR_WALLET_ADDRESS&slippage=1"
-
-            buy_response = requests.get(buy_url)
-            sell_response = requests.get(sell_url)
-
-            if buy_response.status_code == 200 and sell_response.status_code == 200:
-                return True
+            buy_hash = self.swap_tokens(self.wallet_address, self.private_key, self.native_token, address, amount)
+            time.sleep(3)
+            if buy_hash:
+                sell_hash = self.swap_tokens(self.wallet_address, self.private_key, self.native_token, address, amount)
+                time.sleep(3)
+                if sell_hash:
+                    return True
+                else:
+                    return False
             else:
                 return False
-        except Exception as e:
-            print(f"Error testing token {token_address}: {e}")
+        except Exception:
             return False
 
-    def get_token_info(self, address, chain_id):
-
-        method = "get"
-        apiUrl = f"https://api.1inch.dev/token/v1.2/{chain_id}/search"
-        requestOptions = {
-            "headers": {
-                "Authorization": f"Bearer {self.api_key}"
-            },
-            "body": {},
-            "params": {
-                "query": address,
-                "ignore_listed": "false",
-                "only_positive_rating": "false"
-            }
-        }
-
-        # Prepare request components
-        headers = requestOptions.get("headers", {})
-        body = requestOptions.get("body", {})
-        params = requestOptions.get("params", {})
-
-        response = requests.get(apiUrl, headers=headers, params=params)
-        return response.json()
-
-
-    def check_wallet_assets(self, wallet_address, chain_id='1'):
+    def check_wallet_assets(self):
         """Fetches assets for a given wallet address using the 1inch API.
 
         Args:
@@ -70,7 +49,7 @@ class OneInchAPI:
             dict: A dictionary containing the assets in the wallet.
         """
         method = "get"
-        apiUrl = f"https://api.1inch.dev/balance/v1.2/{chain_id}/balances/{wallet_address}"
+        apiUrl = f"https://api.1inch.dev/balance/v1.2/{self.chain_id}/balances/{self.wallet_address}"
         requestOptions = {
             "headers": {
                 "Authorization": f"Bearer {self.api_key}"
@@ -90,51 +69,50 @@ class OneInchAPI:
         else:
             raise Exception(f"Failed to fetch wallet assets: {response.text}")
 
-
     def __str__(self) -> str:
         return f"OneInchAPI(api_key={self.api_key})"
 
     def get_swap_rate(self, to_token_address: str,
                       from_token_address: str = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-                      amount: int = 10 ** 12, network: int = 250) -> dict:
+                      amount: int = 10 ** 18) -> dict:
         """
         Retrieves the swap rate for a given pair of tokens.
 
         :param from_token_address: Address of the source token.
         :param to_token_address: Address of the destination token.
         :param amount: Amount of the source token to swap.
-        :param network: Network ID (Chain ID).
         :return: Dictionary with price and gas amount or None if request fails.
         """
         headers = {"Authorization": f"Bearer {self.api_key}", "accept": "application/json"}
         params = {
-            "chain": network,
             "src": from_token_address,
             "dst": to_token_address,
             "amount": amount,
             "includeTokensInfo": "true",
-            "includeGas": "true",
+            "includeProtocols": "false",
+            "includeGas": "true"
         }
-        api_url = f"https://api.1inch.dev/swap/v5.2/{network}/quote"
+        api_url = f"https://api.1inch.dev/swap/v6.0/{self.chain_id}/quote"
 
         try:
             response = requests.get(api_url, headers=headers, params=params)
             if response.status_code == 200:
                 data = response.json()
-                price = data['toAmount']
+                price = data['dstAmount']
                 return {"price": price, "gas": Decimal(data.get("gas", 0)), "name": data.get("name")}
             else:
                 return None
-        except Exception:
+        except Exception as e:
             return None
 
-    def get_chain_pairs_prices(self, chain_id: int) -> str:
+    def get_chain_pairs_prices(self) -> str:
         """
         Retrieves available token pairs for a given chain.
 
         :param chain_id: Network ID (Chain ID) for which to retrieve token pairs.
         :return: Response text containing token pairs or error message.
         """
+        chain_id = self.chain_id
         headers = {"Authorization": f"Bearer {self.api_key}", "accept": "application/json"}
         params = {"chain": chain_id, "currency": "USD"}
         api_url = f"https://api.1inch.dev/price/v1.1/{chain_id}"
@@ -145,7 +123,8 @@ class OneInchAPI:
         except Exception:
             return "Error fetching chain pairs"
 
-    def get_chain_pairs(self, chain_id: int) -> str:
+    def get_chain_pairs(self) -> str:
+        chain_id = self.chain_id
         method = "get"
         api_url = f"https://api.1inch.dev/token/v1.2/{chain_id}"
         requestOptions = {
@@ -167,18 +146,11 @@ class OneInchAPI:
         except Exception:
             return "Error fetching chain pairs"
 
-    def swap_tokens(self, from_address, private_key, from_token_address, to_token_address, amount, end_point):
-        web3_instance = Web3Instance.get_instance(end_point).web3
-
-        # Normalize the from_address to checksum address
-        from_address = web3_instance.to_checksum_address(from_address)
+    def swap_tokens(self, from_address, private_key, from_token_address, to_token_address, amount):
+        web3_instance = Web3Instance.get_instance(self.end_point).web3
 
         # 1inch API endpoint for swap
-        api_url = f"https://api.1inch.dev/swap/v6.0/137/swap"
-
-        headers = {
-            'Authorization': "Bearer " + self.api_key
-        }
+        api_url = "https://api.1inch.io/v4.0/1/swap"
 
         # Prepare swap request
         params = {
@@ -189,41 +161,13 @@ class OneInchAPI:
             'slippage': 1,
         }
 
-        response = requests.get(api_url, headers=headers, params=params)
-
-        if response.status_code != 200:
-            raise Exception(f"Error: Received status code {response.status_code} from API. Response: {response.text}")
-
-        try:
-            response_json = response.json()
-        except ValueError:
-            raise Exception(f"Error: Could not parse response as JSON. Response: {response.text}")
+        response = requests.get(api_url, params=params).json()
 
         # Extracting transaction data
-        if 'tx' not in response_json:
-            raise Exception(f"Error: 'tx' field not found in response. Response: {response_json}")
-
-        tx = response_json['tx']
-
-        # Ensure the 'from' field in the transaction data matches the normalized from_address
-        tx['from'] = from_address
-        tx['to'] = web3_instance.to_checksum_address(tx['to'])
-
-        # Fetch the current nonce for the from_address
-        nonce = web3_instance.eth.get_transaction_count(from_address)
-        tx['nonce'] = nonce
-
-        # Convert necessary fields to appropriate types
-        tx['value'] = int(tx['value'])
-        tx['gasPrice'] = int(tx['gasPrice'])
-        tx['chainId'] = web3_instance.eth.chain_id
+        tx = response['tx']
 
         # Sign and send the transaction
         signed_tx = web3_instance.eth.account.sign_transaction(tx, private_key=private_key)
         tx_hash = web3_instance.eth.send_raw_transaction(signed_tx.rawTransaction)
-        return tx_hash.hex()
-        # Sign and send the transaction
-        signed_tx = web3_instance.eth.account.sign_transaction(tx, private_key=private_key)
-        tx_hash = web3_instance.eth.send_raw_transaction(signed_tx.rawTransaction)
-        return tx_hash.hex()
 
+        return tx_hash.hex()
